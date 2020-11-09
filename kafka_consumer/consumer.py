e1cea8e27e36bb4e4327985ddc7b05c0007c7525
Florian Redmann <Florian.Redmann@rewe-group.com>
2020-11-09 13:38:22 +0100

    consumer changes

--- kafka_consumer/consumer.py
+++ kafka_consumer/consumer.py
@@ -2,24 +2,19 @@ from kafka import KafkaConsumer, KafkaProducer
 from pymongo import MongoClient
 from bson.json_util import loads
 
-if __name__ == '__main__':
+# database
+client = MongoClient('mongodb://localhost:27017')
+db = client['twitter']
+collection = db['tweets']
 
-    # database
-    client = MongoClient('mongodb://localhost:27017')
-    db = client['twitter']
-    collection = db['tweets']
+# kafka
+topic_name = 'json-topic'
+consumer = KafkaConsumer(topic_name, 
+    auto_offset_reset='earliest', 
+    bootstrap_servers=['localhost:9092'], 
+    api_version=(0, 10), 
+    consumer_timeout_ms=10000)
 
-    # kafka
-    topic_name = 'json-topic'
-    consumer = KafkaConsumer(topic_name, 
-        auto_offset_reset='earliest', 
-        bootstrap_servers=['localhost:9092'], 
-        api_version=(0, 10), 
-        consumer_timeout_ms=10000)
-
-    # kafka to mongo
-    for msg in consumer:
-        collection.insert_one(loads(msg.value))
-
-    # close kafka connection
-    consumer.close()
+# kafka to mongo
+for msg in consumer:
+    collection.insert_one(loads(msg.value))
